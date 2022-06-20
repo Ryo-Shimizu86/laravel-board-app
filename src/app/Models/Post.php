3d111a1653c671427bbd4804d7c9cb1803ac4cb9
Ryo-Shimizu86 <shimizu@1st-link.co.jp>
2022-06-20 13:41:05 +0900

    ユーザ・投稿テーブルのリレーション

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Post extends Model
 {
@@ -12,4 +13,9 @@ class Post extends Model
      * @var string
      */
     protected $table = 'posts';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
