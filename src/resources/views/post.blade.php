65b65be9f942a38e4f60d56c5faa65e739bf3a12
Ryo-Shimizu86 <shimizu@1st-link.co.jp>
2022-06-20 15:08:50 +0900

    長文メッセージ対応

--- src/resources/views/post.blade.php
+++ src/resources/views/post.blade.php
@@ -19,27 +19,33 @@
                 </div>
             </form>
         </div>
-        <div class="container">
-            <div class="row justify-content-start">
-                @foreach ($posts as $post)
-                <div class="col-2">画像</div>
-                <div class="col-10">
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            {{ $post->user->name }}
-                        </h5>
-                        <h6 class="card-subtitle mb-2 text-muted">
-                            {{ $post->updated_at->format('Y/m/d') }}
-                        </h6>
-                        <p class="card-text">
-                            {{ $post->message }}
-                        </p>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-        </div>
-        {{ $posts->links() }}
     </div>
 </div>
+<div class="container">
+    <div class="row justify-content-start">
+        @foreach ($posts as $post)
+        <!-- <div class="col-2">画像</div> -->
+        <div class="col-10">
+            <div class="card-body">
+                <h5 class="card-title me-5">
+                    {{ $post->user->name }}
+                </h5>
+                <h6 class="card-subtitle mb-2 text-muted">
+                    {{ $post->updated_at->format('Y/m/d') }}
+                </h6>
+                <p class="card-text">
+                    <a
+                        href="{{ route('detail', [ 'id'=>$post->id ]) }}"
+                        class="d-inline-block text-truncate message-title"
+                    >
+                        {{ $post->message }}
+                    </a>
+                </p>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    {{ $posts->links() }}
+</div>
+
 @endsection
